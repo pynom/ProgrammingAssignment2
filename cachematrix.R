60999bcd7f28166911571208892da47c34f5e2ae
pynom <23022522+pynom@users.noreply.github.com>
2020-07-03 21:37:18 +0700

    add documentation to answer

--- cachematrix.R
+++ cachematrix.R
@@ -3,41 +3,46 @@
 
 ## Write a short comment describing this function
 
+# Function below will create matrix capable in caching its inverse
+# Starting with set matrix, getting matrix and setup the inverse and getting it
+
 makeCacheMatrix <- function(x = matrix()) {
-		# creates a cached matrix object
-		inv <- NULL
-		# 
-	    set <- function(y) {
-		        x <<- y
-		        inv <<- NULL
-	    }
-	    get <- function(){
-	        	x 
-	    } 
-	    setinv <- function(inverse) {
-	        	inv <<- inverse 
-	    }
-	    getinv <- function() {
-	        	inv 
-	    }
-	    list(set = set, get = get,
-		        setinv = setinv,
-		        getinv = getinv)
+  # creates a cached matrix object
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setinv <- function(inverse) {
+    inv <<- inverse
+  }
+  getinv <- function() {
+    inv
+  }
+  list(set = set, get = get,
+            setinv = setinv,
+            getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
+# Function below will compute the inverse of the special matrix
+# using the output returned by makeCacheMatrix above
+# it will retrieve after computing the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 
-        inv <- x$getinv()
-	    if(!is.null(inv)) {
-		        message("getting cached data")
-		        return(inv)
-	    }
-	    data <- x$get()
-	    inv <- solve(data, ...)
-	    x$setinv(inv)
-	    inv
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
